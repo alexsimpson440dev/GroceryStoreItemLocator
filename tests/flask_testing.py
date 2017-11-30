19a7007fcc89bbf12d9063e91173325c8ccadca3
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-11-29 23:36:03 -0600

    Added another test. This test tests flask on the index route again. But this time, it is testing that actual data is getting loaded.

--- tests/flask_testing.py
+++ tests/flask_testing.py
@@ -1,9 +1,18 @@
+# help from tutorial: https://www.youtube.com/watch?v=WDh_VQ41kYI&ab_channel=MichaelHerman
+
 from app import app
 import unittest
 
 class TestFlask(unittest.TestCase):
 
+    # tests to see if status code is 200 for index
     def test_index(self):
         tester = app.test_client()
         response = tester.get('/index', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+    # tests to see if data actually loads for index page
+    def test_index_loads(self):
+        tester = app.test_client()
+        response = tester.get('/index', content_type='html/text')
+        self.assertTrue(b'store number' in response.data)
