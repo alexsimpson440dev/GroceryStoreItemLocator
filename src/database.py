f95d9ed1885ad92e367f553cba81b1b542af9c3c
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-12-10 21:32:23 -0600

    Updated comments.

--- src/database.py
+++ src/database.py
@@ -6,7 +6,7 @@ from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey, cre
 from sqlalchemy.orm import mapper, sessionmaker, relationship
 
 METADATA = MetaData()
-
+# Database class that handles database mapping, creating tables, creating sessions, and quering data
 class Database():
     def __init__(self, connection_string="sqlite:///foodLocationDB.sqlite3"):
         self.sqlite_file = connection_string
@@ -16,6 +16,7 @@ class Database():
         self.location = self._map_location()
         METADATA.create_all(bind=self.engine)
 
+    # creates and maps the store data to its data model
     def _map_store(self):
         store = Table('Store', METADATA,
             Column('store_id', Integer, primary_key=True),
@@ -24,6 +25,7 @@ class Database():
         mapper(Store, store)
         return store
 
+    # creates and maps the items data to its data model
     def _map_items(self):
         items = Table('Items', METADATA,
             Column('item_id', Integer, primary_key=True),
@@ -34,6 +36,7 @@ class Database():
         mapper(Items, items)
         return items
 
+    # creates and maps the location table to its data model
     def _map_location(self):
         location = Table('Location', METADATA,
                          Column('location_id', Integer, primary_key=True),
@@ -41,30 +44,36 @@ class Database():
         mapper(Location, location, properties={'loc_id': relationship(Items, backref='Location')})
         return location
 
-
+    # gets a connection to the database via the file, which is linked to the connection string to find its location
     def _get_connection(self):
         engine = create_engine(self.sqlite_file)
         return engine
 
+    # creates a session based off the engine that was created in the _get_connection
     def _get_session(self):
         session = sessionmaker(bind=self.engine)
         return session()
 
+    # adds a store to the store table, commits changes
     def _add_store(self, store):
         session = self._get_session()
         session.add(store)
         session.commit()
 
+    # adds an items to the items table, commits changes
     def _add_item(self, item):
         session = self._get_session()
         session.add(item)
         session.commit()
 
+    # adds an item location to the location table, commits changes
     def _add_location(self, location_name):
         session = self._get_session()
         session.add(location_name)
         session.commit()
 
+    #todo: combine this query and query below into one
+    # queries the store table for the store id where the given data is equal to the store id
     def _search_store(self, store_id):
         session = self._get_session()
         for store in session.query(Store)\
@@ -75,12 +84,14 @@ class Database():
             else:
                 return store.image_dir
 
+    # queries the store table for the store name where the store id is equal to the data given
     def _search_store_name(self, store_id):
         session = self._get_session()
         for store in session.query(Store)\
             .filter(Store.store_id == store_id):
             return store.store_name
 
+    # queries the items table for the item name and brand to display to user
     def _search_store_items(self, search_item):
         items_list = list()
         session = self._get_session()
--- src/dbManager.py
+++ src/dbManager.py
@@ -3,30 +3,37 @@ from src.store import Store
 from src.items import Items
 from src.location import Location
 
+# class that manages requests from the server to the database
 class Manager():
     def __init__(self):
         self.database = Database()
 
+    # sends data to add a store to database
     def add_store(self, store_id, store_name, image_dir):
         store = Store(store_id, store_name, image_dir)
         self.database._add_store(store)
 
+    # sends data to add an items to the database
     def add_item(self, item_brand, item_name, item_category, loc_id):
         item = Items(item_brand, item_name, item_category, loc_id)
         self.database._add_item(item)
 
+    # sends data to add an item location to database
     def add_location(self, location_name):
         loc = Location(location_name)
         self.database._add_location(loc)
 
+    # sends data to search the store table for the map directory
     def get_store_image(self, store_id):
         picture = self.database._search_store(store_id)
         return picture
 
+    # sends data to get the store name from the store table
     def get_store_name(self, store_id):
         name = self.database._search_store_name(store_id)
         return name
 
+    # sends data to search an item by its name
     def search_items_by_name(self, search_item):
         returned_items = self.database._search_store_items(search_item)
         return returned_items
